2f88e47f696b285a29edc3ce683ee2eb7bb54862
Then19 <tol9hstar@gmail.com>
2021-12-28 22:59:13 +0500

    some change again

--- handlers.py
+++ handlers.py
@@ -1,5 +1,5 @@
 from main import bot, dp, anti_flood
-from aiogram.types import Message, CallbackQuery, InputMedia, InputFile
+from aiogram.types import Message, CallbackQuery
 from config import ADMIN_ID
 import sqlConnect
 import messages.msg as msg
@@ -17,13 +17,6 @@ async def sand_to_admin(dp):
 async def get_menu_back(user_id):
     user = sqlConnect.get_user_info(user_id)
     return await bot.send_photo(user_id, photo=main_render(user_id, *user), reply_markup=kb_il.inline_menu_kb)
-
-
-async def get_menu_btn(user_id, message_id):
-    user = sqlConnect.get_user_info(user_id)
-    file = InputMedia(media=InputFile(main_render(user_id, *user)))
-    return await bot.edit_message_media(chat_id=user_id, message_id=message_id,
-                                        media=file, reply_markup=kb_il.inline_menu_kb)
 
 
 @dp.message_handler(commands=['start'])
@@ -76,7 +69,8 @@ async def out_cash_btn(callback_query: CallbackQuery):
 @dp.throttled(anti_flood, rate=1)
 async def back_to_menu(callback_query: CallbackQuery):
     user_id = callback_query.from_user.id
-    return await get_menu_btn(user_id, callback_query.message.message_id)
+    await bot.delete_message(user_id, message_id=callback_query.message.message_id)
+    return await get_menu_back(user_id)
 
 
 # ________________________________________ END _______________________________________________________________
